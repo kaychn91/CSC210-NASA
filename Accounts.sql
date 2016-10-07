21af3c1c78a03eb306009e65b603266d49eda01f
kaychn91 <annie.he91@gmail.com>
2016-10-07 18:21:12 -0400

    Add files via upload

---
+++ Accounts.sql
@@ -0,0 +1,10 @@
+CREATE TABLE account {
+	username VARCHAR(30) UNIQUE PRIMARY KEY
+	password VARCHAR(30)
+	Fname VARCHAR
+	LName VARCHAR
+	email VARCHAR 
+	DOB DATE
+	gender CHAR(1)
+	FavGame VARCHAR
+}
