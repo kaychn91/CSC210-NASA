5d20368283a5403b918560431a725c33f51ba3d2
SamMeBe <sbahr@u.rochester.edu>
2016-11-03 19:18:19 -0400

    Update check_insert_user.py

--- www/cgi-bin/check_insert_user.py
+++ www/cgi-bin/check_insert_user.py
@@ -10,7 +10,7 @@ import MySQLdb as mdb
 def create_database():
     conn = mdb.connect(db='csc210',host='localhost',user='root',passwd='mysql')
     cursor = conn.cursor()
-    cursor.execute("CREATE TABLE IF NOT EXISTS csc210.Accounts(username varchar(30) primary key, password varchar(30), salt varchar(100), fName varchar(50), lName varchar(50), email varchar(100),dob date,gender char(1), favGame varchar(100)) ENGINE = InnoDB;") 
+    cursor.execute("CREATE TABLE IF NOT EXISTS csc210.Accounts(username varchar(30) primary key, password varchar(100), salt varchar(100), fName varchar(50), lName varchar(50), email varchar(100),dob date,gender char(1), favGame varchar(100)) ENGINE = InnoDB;") 
     conn.commit()
     conn.close()
 
